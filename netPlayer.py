12f127ad59ba0de956da1d87c9237a435a36aa48
arnav-gudibande <arnav.gudibande@gmail.com>
2016-07-06 16:11:27 -0700

    Updated netplayer

--- netPlayer.py
+++ netPlayer.py
@@ -3,14 +3,19 @@ import nengo
 import cellular
 import continuous
 import body
+import pacman
 
 class NetPlayer(nengo.Network):
 
     def __init__(self, **kwargs):
 
-        super(self, NetPlayer).__init__(**kwargs)
+        super(self, NetPlayer).__init__(N, **kwargs)
+
+        self.player_move = []
 
         self.move = nengo.Node(move, size_in = 2, size_out = 2)
         self.food = nengo.Node(food, size_in = 2, size_out = 2)
         self.obstacles = nengo.Node(obstacles, size_in = 2, size_out = 2)
-        self.sensor_in = nengo.Node(lambda t,x: x, size_in = 2, size_out = 2)
+
+        for i in xrange(N):
+            self.player_move.append()
